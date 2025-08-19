a5c8fecb18785bfd380628822cdad1c8ce6076ea
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-08-19 19:21:25 +0800

    Added test page

--- server/routes/v1/admin.py
+++ server/routes/v1/admin.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, request
+from flask import Blueprint, render_template
 
 admin_router = Blueprint("admin_router", __name__)
 
 @admin_router.route("/")
 def login_page():
-    return "<h1>Admin login page</h1>"
+    return render_template("admin/admin.html")
