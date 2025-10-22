f2fbf444df4edf8b477a08a104f3ed12da8e877c
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-10-22 15:02:52 +0800

    :hammer: Refactored the captive portal structure

--- public/components/captivePortal/main.js
+++ public/components/captivePortal/main.js
@@ -3,8 +3,6 @@ export default function Main(root){
         <h1> Welcome to S.M.I.L.E. Wifi!<h1>
         <button id="start-drop">🚮 Press Me to Start Drop</button>
         <p id="earned">🕒 Waiting...</p>
-        <form action="/v1/splash/authenticate" method="post">
-            <button id="authBtn" type="submit" disabled>Click to Get Internet</button>
-        </form>
+        <button id="auth-btn" disabled>Click to Get Internet</button>
     `;
 }
