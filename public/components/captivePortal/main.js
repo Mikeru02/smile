e0f9bd6a88375e8508a099c62c8187f821912963
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-10-23 12:54:28 +0800

    :hammer: Improve UI for captive portal

--- public/components/captivePortal/main.js
+++ public/components/captivePortal/main.js
@@ -4,14 +4,18 @@ import styles from './component.module.css';
 export default function Main(root){
     root.innerHTML = `
         <div class="${styles["card"]}">
-            <img src=${Logo}>
-            <h1>S.M.I.L.E</h1>
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name" placeholder="Name">
-            <label for="course">Course / Strand</label>
-            <input type="text" name="course" id="course" placeholder="Course/Strand">
-            <label for="yearlvl">Year Level</label>
-            <input type="text" name="yearlvl" id="yearlvl" placeholder="Year Level"
+            <img src=${Logo} class="${styles["logo"]}">
+            <h1 class="${styles["title"]}">S.M.I.L.E</h1>
+            <p class="${styles["desc"]}">Input Credentials</p>
+            <div class="${styles["input-fields"]}">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" placeholder="Name">
+                <label for="course">Course / Strand</label>
+                <input type="text" name="course" id="course" placeholder="Course/Strand">
+                <label for="yearlvl">Year Level</label>
+                <input type="text" name="yearlvl" id="yearlvl" placeholder="Year Level">
+            </div>
+            <button id="submit-credentital" class="${styles["button-submit-cred"]}">SUBMIT</button>
         </div>
     `;
 
