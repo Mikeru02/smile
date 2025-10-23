21d42a15988af9dafa9ffe01062968bfb5c1e4f2
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-10-23 09:47:14 +0800

    :hammer: Added layouts

--- public/components/captivePortal/main.js
+++ public/components/captivePortal/main.js
@@ -1,8 +1,19 @@
+import Logo from '/icons/logo.svg';
+import styles from './component.module.css';
+
 export default function Main(root){
     root.innerHTML = `
-        <h1> Welcome to S.M.I.L.E. Wifi!<h1>
-        <button id="start-drop">🚮 Press Me to Start Drop</button>
-        <p id="earned">🕒 Waiting...</p>
-        <button id="auth-btn" disabled>Click to Get Internet</button>
+        <div class="${styles["card"]}">
+            <img src=${Logo}>
+            <h1>S.M.I.L.E</h1>
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" placeholder="Name">
+            <label for="course">Course / Strand</label>
+            <input type="text" name="course" id="course" placeholder="Course/Strand">
+            <label for="yearlvl">Year Level</label>
+            <input type="text" name="yearlvl" id="yearlvl" placeholder="Year Level"
+        </div>
     `;
+
+    root.className = styles["main"];
 }
