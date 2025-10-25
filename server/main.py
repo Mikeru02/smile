961eef9e44c69e1765682d88ba78fa368c885745
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-10-25 16:41:44 +0800

    Added helpers modules

--- server/main.py
+++ server/main.py
@@ -1,4 +1,30 @@
-from app import app
+from modules.helpers.service_checker import Service
+from modules.helpers.setup_config import SETUP
+from modules.helpers.git_checker import GIT
 
 if __name__ == "__main__":
+    # Check and restart the services needed
+    services = ["dnsmasq", "NetworkManager"]
+    for service in services:
+        print(f"Checking service [{service}]")
+        check = Service.check(service)
+        if check == False:
+            print(f"Service [{service}] is down. Restarting...")
+            restart = Service.restart(service)
+            print(f"Service [{service}] is {restart}")
+    
+    # Setup the configuation of iptables and conntrack
+    print("Setting up iptables configuration...")
+    SETUP.flush()
+    SETUP.set()
+
+    # Check fo updates in the repository
+    print("Fetching current updates...")
+    git_result = GIT.fetch()
+    if git_result:
+        GIT.pull()
+
+    # Import updated app here
+    from app import app
+
     app.run(debug=True)
