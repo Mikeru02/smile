89c6b9c19e205a8b89e1ce93b92fc5362350bcea
BlancheTinsleye <bjashleymercado@gmail.com>
2025-11-08 10:03:14 +0800

    script for resizing

---
+++ resize.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from pathlib import Path
+
+'''
+    Pang resize ko ng images para di ko gagawin manually isa-isa.   
+'''
+
+def resize_image(input_path, output_path, size):
+    with Image.open(input_path) as img:
+        img = img.resize(size, Image.Resampling.LANCZOS)
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        img.save(output_path)
+        print(f"DONE ({output_path})")
+
+imgs_folder = Path("to_process/to_resize/nov7/")
+save_folder = Path("to_process/resized/nov8/")
+new_size = (384, 512)
+
+def get_imgs(input_dir, output_dir):
+    for file_path in input_dir.glob("*.jpg"):
+        nm = file_path.name
+        print(f"Processing {nm}...")
+
+        out_path = output_dir / nm
+        resize_image(file_path, out_path, new_size)
+
+    print("All done!")
+
+get_imgs(imgs_folder, save_folder)
