c8e9a4ffecdd7ae92af5de2ebc9fd7c75afa546e
BlancheTinsleye <bjashleymercado@gmail.com>
2025-11-21 10:16:27 +0800

    tweak

--- resize.py
+++ resize.py
@@ -2,7 +2,11 @@ from PIL import Image
 from pathlib import Path
 
 '''
-    Pang resize ko ng images para di ko gagawin manually isa-isa.   
+    Pang resize ko ng images para di ko gagawin manually isa-isa.
+
+    find . -type f -name "*:Zone.Identifier" -delete
+    source venv/bin/activate
+    label-studio start
 '''
 
 def resize_image(input_path, output_path, size):
@@ -12,14 +16,14 @@ def resize_image(input_path, output_path, size):
         img.save(output_path)
         print(f"DONE ({output_path})")
 
-imgs_folder = Path("to_process/to_resize/nov7/")
-save_folder = Path("to_process/resized/nov8/")
+imgs_folder = Path("to_process/to_resize/kalat/")
+save_folder = Path("to_process/resized/nov15/")
 new_size = (384, 512)
 
 def get_imgs(input_dir, output_dir):
     for file_path in input_dir.glob("*.jpg"):
         nm = file_path.name
-        print(f"Processing {nm}...")
+        # print(f"Processing {nm}...")
 
         out_path = output_dir / nm
         resize_image(file_path, out_path, new_size)
