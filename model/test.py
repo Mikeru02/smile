7f3b306b0b2f801db57ea35b439df0c46582fe9a
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-11-29 13:55:55 +0800

    Updated model

--- model/test.py
+++ model/test.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
 # model = YOLO("model/yolo_smile/weights/best.pt")
-model = YOLO("model/models/yolo_smile_v1-0-pbp.pt")
+model = YOLO("model/models/yolo_smile_v1-1-2.pt")
 
 results = model.predict(
     source="model/datasets/images/val",
     imgsz=512,
-    conf=0.1,
+    conf=0.75,
     save=True,
     save_txt=True
 )
