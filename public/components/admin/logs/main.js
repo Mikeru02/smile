008bedcf4c46e511ddd593a25c09a92cbf30eeb0
RodienJillian <rodiengorg@gmail.com>
2025-12-01 22:44:01 +0800

    🛠️

--- public/components/admin/logs/main.js
+++ public/components/admin/logs/main.js
@@ -2,15 +2,19 @@ import styles from "./component.module.css";
 
 export default function MainContent(root) {
     root.innerHTML = `
-        <table id="analytics-table" class="analytics-table">
-            <thead>
-                <tr>
-                    <th>Timestamp</th>
-                    <th>Event</th>
-                    <th>Description</th>
-                </tr>
-            </thead>
-            <tbody></tbody>
-        </table>
+        <div class="${styles['logs-container']}">
+            <div class="${styles['logs-table-container']}">
+                <table id="logs-table" class="${styles['logs-table']}">
+                    <thead>
+                        <tr>
+                            <th>Timestamp</th>
+                            <th>Event</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+        </div>
     `;
 }
