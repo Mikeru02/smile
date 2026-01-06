c50c4a35c992d6ce1c32a6bfd257a7cbd2ef6686
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-01-06 10:09:24 +0800

    Added ip filtering

--- server/modules/utils/get_all_ip.py
+++ server/modules/utils/get_all_ip.py
@@ -1,9 +1,13 @@
 import subprocess
 
-def get_all_ip():
+def get_all_ip(ignore_ips=["192.168.10.1", "127.0.0.1"]):
     result = subprocess.run(
         ["hostname", "-I"],
         capture_output=True,
         text=True
     )
-    return result.stdout.strip().split()
+    ips = result.stdout.strip().split()
+
+    filtered_ips = [ip for ip in ips if ip not in ignore_ips]
+
+    return filtered_ips
