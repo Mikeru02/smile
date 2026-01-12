55f4f630f1226585f24031661d6d7bfce95a0432
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-01-12 23:03:59 +0800

    Files ready for api

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import 'dotenv/config.js';
+
+// NOTE: Add the version route here!
+
+const app = express();
+const port = process.env.API_PORT || 4000;
+
+app.use(morgan('combined'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// NOTE: Implement the route versions here!
+
+app.listen(port, () => {
+    console.log(`[RUNNING] API is up and running at port ${port}...`);
+})
