5ff2df18fae833429b370bed39bf6156778e9a5d
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-01-16 19:14:32 +0800

    Updated rules

--- src/utils/iptablesSetup.js
+++ src/utils/iptablesSetup.js
@@ -1,11 +1,11 @@
 import runSpawnSync from './runSpawnSync.js';
 
-const routerAddress = process.env.ROUTER_ADDRESS;
+const routerAddress = process.env.ROUTER_ADDRESS || '192.168.10.1';
 const routerPrimaryInterface = process.env.PRIMARY_INTERFACE;
 const routerSecondaryInterface = process.env.SECONDARY_INTERFACE;
-const tcpPort = Number(process.env.TCP_PORT);
-const udpPort = Number(process.env.UDP_PORT);
-const srcPort = Number(process.env.SRC_PORT);
+const tcpPort = Number(process.env.TCP_PORT) || 80;
+const udpPort = Number(process.env.UDP_PORT) || 53;
+const srcPort = Number(process.env.SRC_PORT) || 80;
 
 export default class IPTSetup {
     static flush() {
@@ -29,12 +29,12 @@ export default class IPTSetup {
 
     static set() {
         const commands = [
-            ['iptables', ['-t', 'nat', '-A', 'PREROUTING', '-i', routerSecondaryInterface, '-p', 'tcp', '--dport', tcpPort, '-j', 'DNAT', '--to-destination', `${routerAddress}:${srcPort}`]],
-            ['iptables', ['-t', 'nat', '-A', 'PREROUTING', '-i', routerSecondaryInterface, '-p', 'udp', '--dport', udpPort, '-j', 'DNAT', '--to-destination', routerAddress]],
-            ['iptables', ['-t', 'nat', '-A', 'POSTROUTING', '-o', routerPrimaryInterface, '-j', 'MASQUERADE']],
-            ['iptables', ['-A', 'FORWARD', '-i', routerSecondaryInterface, '-j', 'DROP']],
-            ['iptables', ['-A', 'FORWARD', '-i', routerSecondaryInterface, '-d', routerAddress, '-j', 'ACCEPT']],
-            ['iptables', ['-A', 'FORWARD', '-i', routerSecondaryInterface, '-p', 'udp', '--dport', udpPort, '-d', routerAddress, '-j', 'ACCEPT']]
+            ["iptables", ["-t", "nat", "-A", "PREROUTING", "-i", routerSecondaryInterface, "-p", "tcp", "--dport", tcpPort, "-j", "DNAT", "--to-destination", `${routerAddress}:${srcPort}`]],
+            ["iptables", ["-t", "nat", "-A", "PREROUTING", "-i", routerSecondaryInterface, "-p", "udp", "--dport", udpPort, "-j", "DNAT", "--to-destination", routerAddress]],
+            ["iptables", ["-t", "nat", "-A", "POSTROUTING", "-o", routerPrimaryInterface, "-j", "MASQUERADE"]],
+            ["iptables", ["-A", "FORWARD", "-i", routerSecondaryInterface, "-o", routerPrimaryInterface, "-j", "ACCEPT"]],
+            ["iptables", ["-A", "FORWARD", "-i", routerPrimaryInterface, "-o", routerSecondaryInterface, "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"]]
+
         ];
 
         for (const [cmd, args] of commands) {
