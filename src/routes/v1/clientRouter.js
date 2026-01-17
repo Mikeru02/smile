cbbe193add98978350dde4473816b78de562ac49
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-01-17 08:13:17 +0800

    Added zero logic

--- src/routes/v1/clientRouter.js
+++ src/routes/v1/clientRouter.js
@@ -44,7 +44,7 @@ clientRouter.post('/disconnect', async (req, res) => {
 
 clientRouter.post('/revoke', async (req, res) => {
     await axios.patch(
-        `http://${process.env.API_HOST}:${process.env.API_PORT}/${process.env.API_ROUTE_VERSION}/client/deauth`, 
+        `http://${process.env.API_HOST}:${process.env.API_PORT}/${process.env.API_ROUTE_VERSION}/client/revoke`, 
         {}, 
         {
             headers: {
