269c51472beef1009ee1f64c13a12a474d028707
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-01-19 20:52:00 +0800

    Trying new rules

--- src/utils/clientManagement.js
+++ src/utils/clientManagement.js
@@ -1,33 +1,29 @@
 import runSpawnSync from './runSpawnSync.js';
 
 export default class ClientManagement {
-    static isValidIP(ip) {
-        return /^(\d{1,3}\.){3}\d{1,3}$/.test(ip);
-    }
-    
     static allowClient(ip) {
-        runSpawnSync('iptables', [
-            '-t', 'nat', '-I', 'PREROUTING', '1',
-            '-i', 'enxec9a0c1bee94',
-            '-s', ip,
-            '-j', 'ALLOW_INTERNET'
-        ]);
-
-        console.log(`[ALLOW] ${ip}`);
-    }
-
-
-    static revokeClient(ip) {
-        runSpawnSync('iptables', [
-            '-t', 'nat', '-D', 'PREROUTING',
-            '-i', 'enxec9a0c1bee94',
-            '-s', ip,
-            '-j', 'ALLOW_INTERNET'
-        ]);
-
+        // Flush old connections
         runSpawnSync('conntrack', ['-D', '-s', ip]);
         runSpawnSync('conntrack', ['-D', '-d', ip]);
 
-        console.log(`[REVOKE] ${ip}`);
+        // Add the client to the CLIENTS_AUTH chain
+        runSpawnSync('iptables', [
+            '-t', 'nat', '-I', 'CLIENTS_AUTH', '-s', ip, '-j', 'RETURN'
+        ]);
+
+        console.log(`[ALLOW] ${ip} internet enabled`);
+    }
+
+    static revokeClient(ip) {
+        // Remove the client from CLIENTS_AUTH
+        runSpawnSync('iptables', [
+            '-t', 'nat', '-D', 'CLIENTS_AUTH', '-s', ip, '-j', 'RETURN'
+        ]);
+
+        // Flush connections
+        runSpawnSync('conntrack', ['-D', '-s', ip]);
+        runSpawnSync('conntrack', ['-D', '-d', ip]);
+
+        console.log(`[REVOKE] ${ip} internet revoked`);
     }
 }
