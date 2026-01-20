0d4c1d5477cae9586377305df4ce06c1ac401b3e
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-01-20 09:34:43 +0800

    Try new rules

--- src/utils/clientManagement.js
+++ src/utils/clientManagement.js
@@ -2,15 +2,19 @@ import runSpawnSync from './runSpawnSync.js';
 
 export default class ClientManagement {
     static allowClient(ip) {
-        // Stop portal redirect for this client
+        // Bypass portal
         runSpawnSync("iptables", ["-t", "nat", "-I", "PREROUTING", "-s", ip, "-p", "tcp", "--dport", "80", "-j", "RETURN"]);
         runSpawnSync("iptables", ["-t", "nat", "-I", "PREROUTING", "-s", ip, "-p", "udp", "--dport", "53", "-j", "RETURN"]);
 
-        // Allow forwarding to uplink
+        // Forwarding: HTTP/HTTPS/other
         runSpawnSync("iptables", ["-I", "FORWARD", "-s", ip, "-o", "enxec9a0c164fda", "-j", "ACCEPT"]);
         runSpawnSync("iptables", ["-I", "FORWARD", "-d", ip, "-i", "enxec9a0c164fda", "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"]);
 
-        // NAT masquerade for this client
+        // Forwarding: DNS
+        runSpawnSync("iptables", ["-I", "FORWARD", "-s", ip, "-p", "udp", "--dport", "53", "-o", "enxec9a0c164fda", "-j", "ACCEPT"]);
+        runSpawnSync("iptables", ["-I", "FORWARD", "-d", ip, "-p", "udp", "--sport", "53", "-i", "enxec9a0c164fda", "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"]);
+
+        // NAT masquerade
         runSpawnSync("iptables", ["-t", "nat", "-I", "POSTROUTING", "-s", ip, "-o", "enxec9a0c164fda", "-j", "MASQUERADE"]);
 
         console.log(`[ALLOW] ${ip} internet allowed`);
