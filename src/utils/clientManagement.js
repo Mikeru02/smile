74bf8b5de27f9999c4bc42b4764afdb51daf2367
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-01-20 10:27:17 +0800

    Try new rules

--- src/utils/clientManagement.js
+++ src/utils/clientManagement.js
@@ -2,34 +2,61 @@ import runSpawnSync from './runSpawnSync.js';
 
 export default class ClientManagement {
     static allowClient(ip) {
-        // Stop portal redirect for HTTP only (port 80)
-        runSpawnSync("iptables", ["-t", "nat", "-I", "PREROUTING", "-s", ip, "-p", "tcp", "--dport", "80", "-j", "RETURN"]);
+        // BYPASS HTTP PORTAL
+        runSpawnSync("iptables", [
+            "-t", "nat", "-I", "PREROUTING",
+            "-s", ip, "-p", "tcp", "--dport", "80",
+            "-j", "RETURN"
+        ]);
 
-        // Forward all traffic from this client
-        runSpawnSync("iptables", ["-I", "FORWARD", "-s", ip, "-o", "enxec9a0c164fda", "-j", "ACCEPT"]);
-        runSpawnSync("iptables", ["-I", "FORWARD", "-d", ip, "-i", "enxec9a0c164fda", "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"]);
+        // BYPASS DNS HIJACK (THIS WAS MISSING)
+        runSpawnSync("iptables", [
+            "-t", "nat", "-I", "PREROUTING",
+            "-s", ip, "-p", "udp", "--dport", "53",
+            "-j", "RETURN"
+        ]);
 
-        // NAT for client
-        runSpawnSync("iptables", ["-t", "nat", "-I", "POSTROUTING", "-s", ip, "-o", "enxec9a0c164fda", "-j", "MASQUERADE"]);
+        // ALLOW FORWARDING
+        runSpawnSync("iptables", [
+            "-I", "FORWARD",
+            "-s", ip, "-o", "enxec9a0c164fda",
+            "-j", "ACCEPT"
+        ]);
+
+        runSpawnSync("iptables", [
+            "-I", "FORWARD",
+            "-d", ip, "-i", "enxec9a0c164fda",
+            "-m", "state",
+            "--state", "ESTABLISHED,RELATED",
+            "-j", "ACCEPT"
+        ]);
+
+        // NAT
+        runSpawnSync("iptables", [
+            "-t", "nat",
+            "-I", "POSTROUTING",
+            "-s", ip,
+            "-o", "enxec9a0c164fda",
+            "-j", "MASQUERADE"
+        ]);
 
         console.log(`[ALLOW] ${ip} internet allowed`);
     }
 
+
     static revokeClient(ip) {
-        // Restore portal redirect for HTTP
-        runSpawnSync("iptables", ["-t", "nat", "-D", "PREROUTING", "-s", ip, "-p", "tcp", "--dport", "80", "-j", "RETURN"]);
+        runSpawnSync("iptables", ["-t","nat","-D","PREROUTING","-s",ip,"-p","tcp","--dport","80","-j","RETURN"]);
+        runSpawnSync("iptables", ["-t","nat","-D","PREROUTING","-s",ip,"-p","udp","--dport","53","-j","RETURN"]);
 
-        // Remove forwarding
-        runSpawnSync("iptables", ["-D", "FORWARD", "-s", ip, "-o", "enxec9a0c164fda", "-j", "ACCEPT"]);
-        runSpawnSync("iptables", ["-D", "FORWARD", "-d", ip, "-i", "enxec9a0c164fda", "-j", "ACCEPT"]);
+        runSpawnSync("iptables", ["-D","FORWARD","-s",ip,"-o","enxec9a0c164fda","-j","ACCEPT"]);
+        runSpawnSync("iptables", ["-D","FORWARD","-d",ip,"-i","enxec9a0c164fda","-m","state","--state","ESTABLISHED,RELATED","-j","ACCEPT"]);
 
-        // Remove NAT
-        runSpawnSync("iptables", ["-t", "nat", "-D", "POSTROUTING", "-s", ip, "-o", "enxec9a0c164fda", "-j", "MASQUERADE"]);
+        runSpawnSync("iptables", ["-t","nat","-D","POSTROUTING","-s",ip,"-o","enxec9a0c164fda","-j","MASQUERADE"]);
 
-        // Kill active connections
-        runSpawnSync("conntrack", ["-D", "-s", ip]);
-        runSpawnSync("conntrack", ["-D", "-d", ip]);
+        runSpawnSync("conntrack", ["-D","-s",ip]);
+        runSpawnSync("conntrack", ["-D","-d",ip]);
 
         console.log(`[REVOKE] ${ip} internet revoked`);
     }
+
 }
