812c76654f52866fcc9abb2621a5bf472018ae47
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-01-21 09:57:12 +0800

    Modified rules

--- src/utils/clientManagement.js
+++ src/utils/clientManagement.js
@@ -1,5 +1,7 @@
 import runSpawnSync from './runSpawnSync.js';
 
+const routerPrimaryInterface = process.env.PRIMARY_INTERFACE;
+
 export default class ClientManagement {
     static allowClient(ip) {
         // Stop portal redirect
@@ -8,11 +10,11 @@ export default class ClientManagement {
         runSpawnSync("iptables", ["-t", "nat", "-I", "PREROUTING", "-s", ip, "-p", "udp", "--dport", "53", "-j", "RETURN"]);
 
         // Forward all traffic
-        runSpawnSync("iptables", ["-I", "FORWARD", "-s", ip, "-o", "enxec9a0c164fda", "-j", "ACCEPT"]);
-        runSpawnSync("iptables", ["-I", "FORWARD", "-d", ip, "-i", "enxec9a0c164fda", "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"]);
+        runSpawnSync("iptables", ["-I", "FORWARD", "-s", ip, "-o", routerPrimaryInterface, "-j", "ACCEPT"]);
+        runSpawnSync("iptables", ["-I", "FORWARD", "-d", ip, "-i", routerPrimaryInterface, "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"]);
 
         // NAT
-        runSpawnSync("iptables", ["-t", "nat", "-I", "POSTROUTING", "-s", ip, "-o", "enxec9a0c164fda", "-j", "MASQUERADE"]);
+        runSpawnSync("iptables", ["-t", "nat", "-I", "POSTROUTING", "-s", ip, "-o", routerPrimaryInterface, "-j", "MASQUERADE"]);
 
         console.log(`[ALLOW] ${ip} internet allowed`);
     }
@@ -24,11 +26,11 @@ export default class ClientManagement {
         runSpawnSync("iptables", ["-t", "nat", "-D", "PREROUTING", "-s", ip, "-p", "udp", "--dport", "53", "-j", "RETURN"]);
 
         // Remove forwarding
-        runSpawnSync("iptables", ["-D", "FORWARD", "-s", ip, "-o", "enxec9a0c164fda", "-j", "ACCEPT"]);
-        runSpawnSync("iptables", ["-D", "FORWARD", "-d", ip, "-i", "enxec9a0c164fda", "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"]);
+        runSpawnSync("iptables", ["-D", "FORWARD", "-s", ip, "-o", routerPrimaryInterface, "-j", "ACCEPT"]);
+        runSpawnSync("iptables", ["-D", "FORWARD", "-d", ip, "-i", routerPrimaryInterface, "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"]);
 
         // Remove NAT
-        runSpawnSync("iptables", ["-t", "nat", "-D", "POSTROUTING", "-s", ip, "-o", "enxec9a0c164fda", "-j", "MASQUERADE"]);
+        runSpawnSync("iptables", ["-t", "nat", "-D", "POSTROUTING", "-s", ip, "-o", routerPrimaryInterface, "-j", "MASQUERADE"]);
 
         // Kill active connections
         runSpawnSync("conntrack", ["-D", "-s", ip]);
--- src/utils/iptablesSetup.js
+++ src/utils/iptablesSetup.js
@@ -1,7 +1,6 @@
 import runSpawnSync from './runSpawnSync.js';
 
 const routerAddress = process.env.ROUTER_ADDRESS || '192.168.10.1';
-const routerPrimaryInterface = process.env.PRIMARY_INTERFACE;
 const routerSecondaryInterface = process.env.SECONDARY_INTERFACE;
 const tcpPort = Number(process.env.TCP_PORT) || 80;
 const udpPort = Number(process.env.UDP_PORT) || 53;
@@ -10,10 +9,12 @@ const srcPort = Number(process.env.SRC_PORT) || 80;
 export default class IPTSetup {
     static flush() {
         const commands = [
-            ['iptables', ['-F']],
-            ['iptables', ['-t', 'nat', '-F']],
-            ['iptables', ['-X']],
-            ['conntrack', ['-F']]
+            ['iptables', ['-F', 'FORWARD']],
+            ['iptables', ['-t', 'nat', '-F', 'PREROUTING']],
+            ['iptables', ['-t', 'nat', '-F', 'POSTROUTING']],
+            ['conntrack', ['-D', '-s', '192.168.10.0/24']],
+            ['conntrack', ['-D', '-d', '192.168.10.0/24']]
+
         ]
 
         for (const [cmd, args] of commands) {
@@ -29,12 +30,12 @@ export default class IPTSetup {
 
     static set() {
         const commands = [
-            ["sysctl", ["-w", "net.ipv4.ip_forward=1"]],
-            ["iptables", ["-t", "nat", "-A", "PREROUTING", "-i", routerSecondaryInterface, "-p", "tcp", "--dport", tcpPort, "-j", "DNAT", "--to-destination", `${routerAddress}:${srcPort}`]],
-            ["iptables", ["-t", "nat", "-A", "PREROUTING", "-i", routerSecondaryInterface, "-p", "udp", "--dport", udpPort, "-j", "DNAT", "--to-destination", routerAddress]],
-            ["iptables", ["-A", "FORWARD", "-i", routerSecondaryInterface, "-o", routerPrimaryInterface, "-j", "ACCEPT"]],
-            ["iptables", ["-A", "FORWARD", "-i", routerPrimaryInterface, "-o", routerSecondaryInterface, "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"]]
-
+            ['sysctl', ['-w', 'net.ipv4.ip_forward=1']],
+            ['iptables', ['-P', 'FORWARD', 'DROP']],
+            ['iptables', ['-t', 'nat', '-A', 'PREROUTING', '-i', routerSecondaryInterface, '-p', 'tcp', '--dport', tcpPort, '-j', 'DNAT', '--to-destination', `${routerAddress}:${srcPort}`]],
+            ['iptables', ['-t', 'nat', '-A', 'PREROUTING', '-i', routerSecondaryInterface, '-p', 'udp', '--dport', udpPort, '-j', 'DNAT', '--to-destination', routerAddress]],
+            ['iptables', ['-A', 'FORWARD', '-d', routerAddress, '-j', 'ACCEPT']],
+            ['iptables', ['-A', 'FORWARD', '-s', routerAddress, '-m', 'state', '--state', 'ESTABLISHED,RELATED', '-j', 'ACCEPT']],
         ];
 
         for (const [cmd, args] of commands) {
