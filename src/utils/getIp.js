fb88f24e9e43adcbd26e3f77de900d2e4e866d58
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-03 00:07:42 +0800

    Modified IP retrival

--- src/utils/getIp.js
+++ src/utils/getIp.js
@@ -2,10 +2,11 @@ import os from 'os';
 
 export default function getLocalIP() {
     const nets = os.networkInterfaces();
+    const ignoredIPs = ['192.168.10.1'];
 
     for (const name of Object.keys(nets)) {
         for (const net of nets[name]) {
-            if (net.family === 'IPv4' && !net.internal) {
+            if (net.family === 'IPv4' && !net.internal && !ignoredIPs.includes(net.address)) {
                 return net.address;
             }
         }
