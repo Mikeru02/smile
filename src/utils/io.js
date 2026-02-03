2aff32455a02526c5a885cb1798dc5091e1aabbd
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-03 11:07:27 +0800

    Added sockets

--- src/utils/io.js
+++ src/utils/io.js
@@ -2,6 +2,8 @@ import { Server } from 'socket.io';
 
 let io = null;
 
+let activeClient = null;    
+
 export function initSocket(server, arduino) {
     if (io) return io;
 
@@ -14,20 +16,46 @@ export function initSocket(server, arduino) {
 
     io.on('connection', (socket) => {
         console.log('New client connected: ', socket.id);
-
-        // Emit
-        socket.emit('ARDUINO:IR', "HELLO")
         
         // On
         socket.on('DROPPING', () => {
-            console.log('Client drop a trash');
-            arduino.sendCommand('DROPPING');
+            if (activeClient) {
+                socket.emit("DROP:busy", { message: "Another user is dropping" });
+                return;
+            }
+            console.log('Client started DROP: ', socket.id);
+
+            activeClient = socket;
+
+            if (arduino) {
+                arduino.sendCommand("DROPPING");
+            }
         })
 
         socket.on('disconnect', () => {
             console.log('Client disconnected: ', socket.id);
+            if (activeClient === socket) {
+                activeClient = null;
+            }
         })
     });
+
+    if (arduino) {
+        arduino.parser.on("data", (data) => {
+            data = data.trim();
+            if (data === "IR DETECTED") {
+                console.log('IR Detected from Arduino');
+
+                if (activeClient) {
+                    activeClient.emit("ARDUINO:IR", {
+                        detected: true,
+                        time: Date.now(),
+                    });
+                    activeClient = null;
+                }
+            }
+        })
+    }
 
     return io;
 }
