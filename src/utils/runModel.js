054451cbebd97a5cb57a979e004bcec0ee02f82f
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-06 05:24:25 +0800

    Debugging only

--- src/utils/runModel.js
+++ src/utils/runModel.js
@@ -1,5 +1,9 @@
 import { spawn } from "child_process";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export function runModel() {
     return new Promise((resolve, reject) => {
