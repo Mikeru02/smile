4fcd0e797df0d252e0daa1ebf2b119420bf8206d
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-19 11:01:17 +0800

    Correct variables

--- src/utils/model.js
+++ src/utils/model.js
@@ -7,9 +7,9 @@ class Model {
     constructor(env) {
         this.env = env;
         this.timeMap = {
-            "general waste": parseInt(this.env.TIME_GENERAL_WASTE || 1),
-            "plastic bottle": parseInt(this.env.TIME_PLASTIC_BOTTLE || 5),
-            "paper": parseInt(this.env.TIME_PAPER || 2)
+            "General Waste": parseInt(this.env.TIME_GENERAL_WASTE || 1),
+            "Plastic Bottle": parseInt(this.env.TIME_PLASTIC_BOTTLE || 5),
+            "Paper": parseInt(this.env.TIME_PAPER || 2)
         }
         this.baseUrl = `https://${this.env.MODEL_HOST}/${this.env.MODEL_VERSION}/model`;
         this.client = axios.create({
