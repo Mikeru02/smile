fa32ba17480a0920c23e7326c2d1e020247c7ccc
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-20 16:21:05 +0800

    Fixed waste return

--- src/utils/model.js
+++ src/utils/model.js
@@ -55,7 +55,7 @@ class Model {
     async earnedTime() {
         const prediction = await this.predict();
         if (!prediction[0]) {
-            return { category: null, earnedTime: 10}
+            return { category: null, wasteCode: "GWST", earnedTime: 10}
         };
 
         const category = prediction[0].class_name?.toLowerCase();
