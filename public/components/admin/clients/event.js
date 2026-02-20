5f692c7391dbe545c029ede05c3c3607a2b26d86
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-20 23:07:09 +0800

    Debugging

--- public/components/admin/clients/event.js
+++ public/components/admin/clients/event.js
@@ -25,7 +25,7 @@ export default async function Event() {
     let headers = populateHeaders(thead, "all-client");
     populateTable(tbody, clients, headers);
 
-    const seeMore = document.querySelectorAll('#see-more');
+    const seeMore = document.querySelectorAll('.see-more');
     seeMore.forEach(button => {
         button.addEventListener('click', () => {
             console.log('See more clicked');
