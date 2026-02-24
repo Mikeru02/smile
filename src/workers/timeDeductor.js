e233e65fbfa46b211a45ba6be42be82599b2e8f4
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-24 20:10:26 +0800

    Modified client

--- src/workers/timeDeductor.js
+++ src/workers/timeDeductor.js
@@ -24,47 +24,29 @@ export default function startTimeDeductor() {
                 const connectionStart = new Date(client.connection_start_at).getTime();
                 const newExpireAt = new Date(connectionStart + newTimeRemaining * 1000);
                 const formattedExpireAt = newExpireAt.toLocaleString('sv-SE').replace('T', ' ')
-                const newStatus = newTimeRemaining <= 0 ? 'expired' : 'active';
+                const newStatus = newTimeRemaining <= 0 ? 'pending' : 'active';
 
-                if (newStatus === 'expired') {
+                if (newStatus === 'pending') {
                     ClientManagement.revokeClient(client.ip);
-                    await axios.patch(
-                        `http://${process.env.SRC_HOST}:${process.env.SRC_PORT}/api/v1/client/id/${client.id}`,
-                        {
-                            name: client.name,
-                            course: client.course,
-                            year_level: client.yearlevel,
-                            status: "pending",
-                            time_remaining: newTimeRemaining,
-                            time_earned: client.time_earned,
-                            expire_at: formattedExpireAt
-                        }, {
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'apikey': process.env.SRC_KEY,
-                            }
-                        }
-                    )
-                } else {
-
-                    await axios.patch(
-                        `http://${process.env.SRC_HOST}:${process.env.SRC_PORT}/api/v1/client/id/${client.id}`,
-                        {
-                            name: client.name,
-                            course: client.course,
-                            year_level: client.yearlevel,
-                            status: "active",
-                            time_remaining: newTimeRemaining,
-                            time_earned: client.time_earned,
-                            expire_at: formattedExpireAt
-                        }, {
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'apikey': process.env.SRC_KEY,
-                            }
-                        }
-                    )
                 }
+
+                await axios.patch(
+                    `http://${process.env.SRC_HOST}:${process.env.SRC_PORT}/api/v1/client/id/${client.id}`,
+                    {
+                        name: client.name,
+                        course: client.course,
+                        year_level: client.yearlevel,
+                        status: newStatus,
+                        time_remaining: newTimeRemaining,
+                        time_earned: client.time_earned,
+                        expire_at: formattedExpireAt
+                    }, {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'apikey': process.env.SRC_KEY,
+                        }
+                    }
+                )
             }
         } catch (err) {
             console.error('Time Deductor Error: ', err);
