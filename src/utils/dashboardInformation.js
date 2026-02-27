b9a8aaedf4bebd939f2608f8644a6547213fbdfb
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-27 12:09:26 +0800

    Debugging

--- src/utils/dashboardInformation.js
+++ src/utils/dashboardInformation.js
@@ -15,7 +15,7 @@ export function checkInternet() {
 export async function checkModel() {
     if (!checkInternet()) return false;
     const result = await axios.get(
-        `https://${process.env.MODEL_LOCALHOST}:${process.env.MODEL_PORT}/${process.env.MODEL_VERSION}/model/`,
+        `http://${process.env.MODEL_LOCALHOST}:${process.env.MODEL_PORT}/${process.env.MODEL_VERSION}/model/`,
         {
             headers: {
                 "Content-Type": "application/json",
