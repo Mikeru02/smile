0f32281cad959130bdcd4b63aff308329539bd21
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-27 10:20:41 +0800

    Calibration

--- tests/sensors/sensors.ino
+++ tests/sensors/sensors.ino
@@ -23,9 +23,9 @@ const int backward = 1000;
 const int openTime = 250;
 const int closeTime = 700;
 
-const int rightTreshold = 22;
-const int leftTreshold = 22;
-const int frontTreshold = 20;
+const int rightTreshold = 24;
+const int leftTreshold = 24;
+const int frontTreshold = 24;
 const int plasticTreshold = 40;
 const int generalTreshold = 35;
 const int paperTreshold = 38;
