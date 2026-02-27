6311d0ff671469c4315b2e8fa0dd88db7ed93c5d
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-27 21:27:06 +0800

    Added calibration

--- arduinoxsmile/arduinoxsmile.ino
+++ arduinoxsmile/arduinoxsmile.ino
@@ -22,8 +22,8 @@ const int paperEchoPin = 7;
 const int stop = 1500;
 const int forward = 2100;
 const int backward = 1000;
-const int openTime = 250;
-const int closeTime = 700;
+const int openTime = 230;
+const int closeTime = 750;
 
 // Variable needed
 String ip;
