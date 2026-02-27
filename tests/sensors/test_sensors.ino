d44336c5dfa2d3fc38458ed07d3cbe9f686ee878
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-27 09:45:31 +0800

    Added sensor testing

---
+++ tests/sensors/test_sensors.ino
@@ -0,0 +1,124 @@
+#include <Wire.h>
+#include <Servo.h>
+#include <LiquidCrystal_I2C.h>
+
+const int servoLeftPin = 12;
+const int servoRightPin = 13;
+const int trigPin = 8;
+const int sideEcho1 = 9;
+const int sideEcho2 = 10;
+const int topEcho = 11;
+const int binTrigPin = A0;
+const int bottlesEchoPin = 5;
+const int generalEchoPin = 6;
+const int paperEchoPin = 7;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+long readDistance(int echoPin);
+
+void setup() {
+  // servoLeft.attach(servoLeftPin);
+  // servoRight.attach(servoRightPin);
+
+  // servoRight.write(90);
+  // servoLeft.write(90);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(topEcho, INPUT);
+  pinMode(sideEcho1, INPUT);
+  pinMode(sideEcho2, INPUT);
+
+  pinMode(binTrigPin, OUTPUT);
+  pinMode(bottlesEchoPin, INPUT);
+  pinMode(generalEchoPin, INPUT);
+  pinMode(paperEchoPin, INPUT);
+
+  digitalWrite(trigPin, LOW);
+  digitalWrite(binTrigPin, LOW);
+
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Ultrasonic Ready");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop() {
+
+  long topDistance   = readDistance(trigPin, topEcho);
+  delay(60);
+
+  long side1Distance = readDistance(trigPin, sideEcho1);
+  delay(60);
+
+  long side2Distance = readDistance(trigPin, sideEcho2);
+  delay(60);
+
+  long bottlesDistance = readDistance(binTrigPin, bottlesEchoPin);
+  delay(60);
+
+  long generalDistance = readDistance(binTrigPin, generalEchoPin);
+  delay(60);
+
+  long paperDistance = readDistance(binTrigPin, paperEchoPin);
+  delay(60);
+
+  lcd.setCursor(0, 0);
+  lcd.print("T:");
+  lcd.print(topDistance);
+  lcd.print(" S1:");
+  lcd.print(side1Distance);
+  lcd.print(" S2:");
+  lcd.print(side2Distance);
+
+  lcd.setCursor(0, 1);
+  lcd.print("B:");
+  lcd.print(bottlesDistance);
+  lcd.print(" G:");
+  lcd.print(generalDistance);
+  lcd.print(" P:");
+  lcd.print(paperDistance);
+
+  // Object detection threshold (example: 15 cm)
+  // int threshold = 25;
+
+  // if (topDistance < threshold || 
+  //     side1Distance < threshold || 
+  //     side2Distance < threshold) {
+
+  //     lcd.setCursor(10, 1);
+  //     lcd.print("OBJ ");
+  // } else {
+  //     lcd.setCursor(10, 1);
+  //     lcd.print("    ");
+  // }
+
+  delay(200);
+}
+
+long readDistance(int trigPin, int echoPin) {
+
+  // Clear trigger
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Send 10us pulse
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Measure echo time
+  long duration = pulseIn(echoPin, HIGH, 30000); // 30ms timeout
+
+  // Convert to cm
+  long distance = duration * 0.034 / 2;
+
+  if (duration == 0) {
+    return 999; // No object detected
+  }
+
+  return distance;
+}
