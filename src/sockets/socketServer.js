3d35daa291acd302f04c5b43aea2e3d934373378
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-28 14:28:48 +0800

    Modified INO code

--- src/sockets/socketServer.js
+++ src/sockets/socketServer.js
@@ -246,7 +246,7 @@ class SocketServer {
                     const uniqueFilename = `capture_${timeStamp}.jpg`;
                     const savedPath = await this.webcam.capture(uniqueFilename);
 
-                    const testPath = this.webcam.getFilePath("test_capture.jpg");
+                    const testPath = await this.webcam.getFilePath("test_capture.jpg");
                     await fs.copyFile(savedPath, testPath);
 
                     console.log("Unique image saved:", savedPath);
