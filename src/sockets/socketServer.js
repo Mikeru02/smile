69093bd094b00c78db915c6a2dc88e0db18fc8fc
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-28 15:00:54 +0800

    Connect socket

--- src/sockets/socketServer.js
+++ src/sockets/socketServer.js
@@ -257,7 +257,7 @@ class SocketServer {
                     client.clientData.time_earned += response.earnedTime;
                     await this.axiosClient.post(
                         `client/earn`,
-                        { earned_time: response.earnedTime, waste_code: wasteCode },
+                        { earned_time: response.earnedTime, waste_code: response.wasteCode },
                         {
                             headers: {
                                 'token': token
