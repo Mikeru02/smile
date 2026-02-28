829da7ab6db03a1828a98ffc6aa5c29050480b2e
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-02-28 14:36:06 +0800

    Added file system

--- src/resources/webcam.js
+++ src/resources/webcam.js
@@ -1,6 +1,7 @@
 import { spawn } from "child_process";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
