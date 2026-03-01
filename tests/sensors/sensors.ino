c2e36dd2c7a8910c359e5796d5687cde08dcb70e
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-01 10:11:48 +0800

    Debugging

--- tests/sensors/sensors.ino
+++ tests/sensors/sensors.ino
@@ -22,7 +22,7 @@ const int stop = 1500;
 const int forward = 2100;
 const int backward = 1000;
 const int openTime = 250;
-const int closeTime = 700;
+const int closeTime = 750;
 
 const int rightTreshold = 22;
 const int leftTreshold = 22;
