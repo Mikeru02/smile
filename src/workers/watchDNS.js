1cbbb54aecd4c45b1868630c34edccbf7d72d5cc
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-05 03:29:32 +0800

    Fixed client management

--- src/workers/watchDNS.js
+++ src/workers/watchDNS.js
@@ -3,25 +3,26 @@ import jwt from 'jsonwebtoken';
 import { spawn } from "child_process";
 
 export default function watchDnsmasq(logFilePath) {
-    const axiosClient = axios.create({
-        baseURL: `http://${process.env.SRC_HOST}:${process.env.SRC_PORT}/api/v1/`,
-        headers: {
-            'Content-Type': 'application/json',
-            'apikey': process.env.SRC_KEY,
-            'token': jwt.sign({ 'role': role}, process.env.API_SECRET_KEY, { expiresIn: '1m' })
-        }
-    });
-
     if (!logFilePath) {
         console.error("dnsmasq log path is undefined.");
         return;
     }
 
-    const process = spawn("tail", ["-F", logFilePath], {
+    // Create axiosClient here, after process.env is ready
+    const axiosClient = axios.create({
+        baseURL: `http://${process.env.SRC_HOST}:${process.env.SRC_PORT}/api/v1/`,
+        headers: {
+            'Content-Type': 'application/json',
+            'apikey': process.env.SRC_KEY,
+            'token': jwt.sign({ 'role': 'admin'}, process.env.API_SECRET_KEY, { expiresIn: '1m' })
+        }
+    });
+
+    const processTail = spawn("tail", ["-F", logFilePath], {
         stdio: ["ignore", "pipe", "pipe"]
     });
 
-    process.stdout.on("data", (data) => {
+    processTail.stdout.on("data", (data) => {
         const lines = data.toString().split("\n");
 
         lines.forEach(async (line) => {
@@ -34,22 +35,19 @@ export default function watchDnsmasq(logFilePath) {
                     if (clientIP !== "127.0.0.1") {
                         await axiosClient.post(
                             `admin/accessed-links`,
-                            {
-                                "clientIP": clientIP,
-                                "domain": domain
-                            }
-                        )
+                            { clientIP, domain }
+                        );
                     }
                 }
             }
         });
     });
 
-    process.stderr.on("data", (err) => {
+    processTail.stderr.on("data", (err) => {
         console.error("dnsmasq watcher error:", err.toString());
     });
 
-    process.on("close", (code) => {
+    processTail.on("close", (code) => {
         console.error("dnsmasq watcher exited with code:", code);
     });
 
