fcad4625fdb43c78853fec3036119c92f1d71bb6
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-05 01:25:29 +0800

    Removed survey

--- public/components/user/timePortal/event.js
+++ public/components/user/timePortal/event.js
@@ -297,11 +297,11 @@ export default function Events() {
             //     renderEarnTime({ hoursSpan, minSpan, secSpan }, data.timeEarned);
             // })
 
-            const redirectToSurvey = true; //Math.random() < 0.5;
-            if (redirectToSurvey) {
-                window.location.href = 'https://forms.gle/LEGksfLDYJH5Ws9r7';
-                return;
-            }
+            // const redirectToSurvey = true; //Math.random() < 0.5;
+            // if (redirectToSurvey) {
+            //     window.location.href = 'https://forms.gle/LEGksfLDYJH5Ws9r7';
+            //     return;
+            // }
         } else {
             connect.textContent = 'Connect';
             clearInterval(timeRemainingInterval);
