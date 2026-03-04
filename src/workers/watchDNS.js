33698670b20de3ac0c5f66b5ec1708430c3fe455
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-05 06:32:51 +0800

    Revert

--- src/workers/watchDNS.js
+++ src/workers/watchDNS.js
@@ -14,7 +14,7 @@ export default function watchDnsmasq(logFilePath) {
     const recentAccesses = new Map(); // key: `${clientIP}-${domain}`, value: timestamp
 
 
-    const processTail = spawn("tail", ["-F", "--line-buffered", logFilePath], {
+    const processTail = spawn("tail", ["-F", logFilePath], {
         stdio: ["ignore", "pipe", "pipe"]
     });
 
