cc9670b26e34450077b98207f49d42a39057cbff
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-05 10:48:25 +0800

    Added toggle button

--- arduinoxsmile/arduinoxsmile.ino
+++ arduinoxsmile/arduinoxsmile.ino
@@ -511,7 +511,7 @@
         respondAndDisplay("BIN STATUS", status, "BINS:" + status);
       }
       else if (command == "SET_UTILITY_MODE") {
-        isUtilityMode = !value;
+        isUtilityMode = value;
         respondAndDisplay("UTILITY MODE", isUtilityMode ? "ON" : "OFF", "UTILITY_MODE:on");
       }
       else if (command == "CHECK_MODE") {
