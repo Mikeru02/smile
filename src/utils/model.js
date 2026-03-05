219bcf9ab203a7a4cd2f66a350d7dec17b618fe7
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-05 14:36:24 +0800

    Added flag

--- src/utils/model.js
+++ src/utils/model.js
@@ -61,7 +61,7 @@ class Model {
     async earnedTime() {
         const prediction = await this.predict();
         if (!prediction[0]) {
-            return { category: "none", wasteCode: "GWST", earnedTime: 60}
+            return { category: "none", wasteCode: "GWST", earnedTime: 120}
         };
 
         const category = prediction[0].class_name?.toLowerCase();
