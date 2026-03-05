5629814f11e41400dc3dac7c67a34d95c5580df8
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-05 12:51:31 +0800

    Added remove and move

--- src/workers/removeAndMoveClients.js
+++ src/workers/removeAndMoveClients.js
@@ -6,7 +6,7 @@ export default async function removeAndMoveClients() {
     const removeAndMoveLoop = async () => {
         try {
             console.log('Remove and Move Worker starts')
-            const response = axios.get(
+            const response = await axios.get(
                 `http://${process.env.SRC_HOST}:${process.env.SRC_PORT}/api/v1/client/outOfTime`,
                 {
                     headers: {
