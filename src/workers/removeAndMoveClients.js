685c1edd01bf4ebdac943de30a5c300870559c10
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-05 13:05:06 +0800

    Added remove and move

--- src/workers/removeAndMoveClients.js
+++ src/workers/removeAndMoveClients.js
@@ -17,21 +17,22 @@ export default async function removeAndMoveClients() {
             )
 
             const outOfTimeClients = response.data.data;
+            print(outOfTimeClients);
 
-            for (const client of outOfTimeClients) {
-                const response = await axios.patch(
-                    `http://${process.env.SRC_HOST}:${process.env.SRC_PORT}/api/v1/client/move`,
-                    {
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'apikey': process.env.SRC_KEY,
-                            'token': jwt.sign({ ip: client.ip }, process.env.API_SECRET_KEY, {
-                                    expiresIn: '1m'
-                                    })
-                        }
-                    }
-                )
-            }
+            // for (const client of outOfTimeClients) {
+            //     const response = await axios.patch(
+            //         `http://${process.env.SRC_HOST}:${process.env.SRC_PORT}/api/v1/client/move`,
+            //         {
+            //             headers: {
+            //                 'Content-Type': 'application/json',
+            //                 'apikey': process.env.SRC_KEY,
+            //                 'token': jwt.sign({ ip: client.ip }, process.env.API_SECRET_KEY, {
+            //                         expiresIn: '1m'
+            //                         })
+            //             }
+            //         }
+            //     )
+            // }
         }catch (err) {
             console.error('Remove and Move Error: ', err);
         } finally {
