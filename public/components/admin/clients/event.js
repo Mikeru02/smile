512faa827ffc7c279042a8c08c5d223ed92f54f7
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-06 22:48:59 +0800

    Debugging

--- public/components/admin/clients/event.js
+++ public/components/admin/clients/event.js
@@ -14,6 +14,7 @@ export default async function Event() {
         });
 
         const clients = allClients.data.data;
+        console.log("CLIENTS:", clients);
         const table = document.getElementById("clients-table");
         const thead = table.querySelector("thead");
         const tbody = table.querySelector("tbody");
