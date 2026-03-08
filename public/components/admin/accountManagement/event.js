0707850007b583223e34b3ec6eb45b6605aabe66
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-08 18:03:36 +0800

    Added CSV convertion

--- public/components/admin/accountManagement/event.js
+++ public/components/admin/accountManagement/event.js
@@ -61,7 +61,7 @@ export default async function PageEvents() {
             CSVExporter.download(accounts.data.data, "accounts.csv");
         }
         catch (err) {
-
+            
         }
     })
 
