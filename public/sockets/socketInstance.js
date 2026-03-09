c5e19b4772298948324d1ce83587608b12100c42
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-09 14:20:04 +0800

    Debugging

--- public/sockets/socketInstance.js
+++ public/sockets/socketInstance.js
@@ -1,11 +1,10 @@
 import SocketClient from "./socketClient.js";
-import checkToken from "../utils/checkToken.js";
 
 const token = localStorage.getItem("token");
 
 const socketClient = new SocketClient();
 
-if (token && checkToken(token)) {
+if (token) {
     socketClient.connect();
 }
 
