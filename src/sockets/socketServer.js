a1de63944de951d702d0699b7fd30b78cfa2f0d9
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-09 13:41:08 +0800

    Debugging

--- src/sockets/socketServer.js
+++ src/sockets/socketServer.js
@@ -89,7 +89,7 @@ class SocketServer {
                         `account/all`,
                         {
                             headers: {
-                                'token': localStorage.getItem('token')
+                                'token': socket.token
                             }
                         }
                     )
