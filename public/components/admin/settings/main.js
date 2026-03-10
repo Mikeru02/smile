40227d51ee7ea824ad3d1be00bb94de604bc3ca5
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-10 08:56:45 +0800

    Added restore defaults

--- public/components/admin/settings/main.js
+++ public/components/admin/settings/main.js
@@ -82,8 +82,8 @@ export default function MainContent(root) {
 
                         <div class="${styles["setting-item"]}">
                             <div>
-                                <button class="${styles['backup-now']}" id="restore-now">Restore Defaults</button>
-                                <button class="${styles['backup-now']}" id="save-now">Save Settings</button>
+                                <button class="${styles['save-default']}" id="restore-now">Restore Defaults</button>
+                                <button class="${styles['save-default']}" id="save-now">Save Settings</button>
                             </div>
                         </div>
 
