7fb364fb0df19f978bef9a5fa0daac3df54a855d
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-10 17:56:03 +0800

    Added buzzer

--- public/utils/validateInput.js
+++ public/utils/validateInput.js
@@ -1,6 +1,8 @@
 export function validateForm(nameInput, consentCheckbox, studentIdInput, submitBtn) {
     const isNameValid = nameInput.value.trim() !== '';
     const isStudentIDValid = studentIdInput.value.trim() !== '';
+    const studentIdPattern = /^\d{2}-\d{4}$/
+    const isValidId = studentIdPattern.test(studentInput.value);
     const isConsentChecked = consentCheckbox.checked;
-    submitBtn.disabled = !(isNameValid && isStudentIDValid && isConsentChecked);
+    submitBtn.disabled = !(isNameValid && isStudentIDValid && isValidId && isConsentChecked);
 }
