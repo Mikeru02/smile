1f5c817ca209ab1cb53109d98997c60d0ed66d40
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-10 13:19:11 +0800

    Added debugging

--- public/components/admin/clients/event.js
+++ public/components/admin/clients/event.js
@@ -44,7 +44,7 @@ export default async function Event() {
 
     socketClient.off('SPECIFIC_CLIENT');
     socketClient.on('SPECIFIC_CLIENT', (data) => {
-        const client = data.clientData;
+        const client = data.clientData[0];
         console.log(client);
 
         if (nameElement) nameElement.value = client.name || '';
