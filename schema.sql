ae15c794f43f917899c296600700682284444416
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-12 11:42:57 +0800

    Debugging

--- schema.sql
+++ schema.sql
@@ -39,7 +39,7 @@ CREATE TABLE `logs`(
     `timestamp` DATETIME NOT NULL,
     `name` VARCHAR(100) NOT NULL,
     `description` VARCHAR(255) NOT NULL,
-    `level` ENUM('INFO', 'WARN', 'ERR') NOT NULL
+    `level` ENUM('INFO', 'WARN', 'ERROR') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `waste`(
