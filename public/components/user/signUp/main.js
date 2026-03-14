1a11c442e2cec9386d9a7fa4d2a8b837fcfaa1c6
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-14 13:18:15 +0800

    Modified UI Form

--- public/components/user/signUp/main.js
+++ public/components/user/signUp/main.js
@@ -7,8 +7,10 @@ export default function Main(root){
         <div class="${styles["card"]}">
             <img src="${Logo}" class="${styles["logo"]}" id="logo">
             <h1 class="${styles["title"]}">S.M.I.L.E</h1>
-            <p class="${styles["desc"]}">Sign Up Form</p>
-            <p class="${styles["desc"]}">Input Credentials</p>
+            <p class="${styles["desc"]}">
+                <p>Sigup Form</p>
+                <p>Input Credentials</p>
+            </p>
             <!-- From Uiverse.io by Yaya12085 --> 
             <div class="${styles["input-fields"]}" id="input-fields">
                 <label for="username">Username</label>
