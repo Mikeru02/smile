a7e240e55f85101a137fcc1ef49745b4f614c476
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-03-19 15:56:28 +0800

    Added content filtering

--- src/controllers/v1/linkController.js
+++ src/controllers/v1/linkController.js
@@ -103,8 +103,8 @@ class LinkController {
         try {
             const domain = await this.link.getDomain(req.params.id);
             const response = await this.link.deleteProhibitedLink(req.params.id);
-            console.log("DEBUG", domain)
-            ContentFiltering.removeDomain(domain.domain)
+            // console.log("DEBUG", domain)
+            ContentFiltering.removeDomain(domain.link)
             await this.log.create(
                 "Delete Prohibited",
                 `${res.locals.username} deleted domain ${req.params.id}`,
